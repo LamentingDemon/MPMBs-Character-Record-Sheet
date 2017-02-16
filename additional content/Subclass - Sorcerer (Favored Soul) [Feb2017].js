ad6cbff502d585d8517a7024561e091266faa172
LamentingDemon <jpacktor@gmail.com>
2017-02-16 14:38:14 -0500

    Update Subclass - Sorcerer (Favored Soul) [Feb2017].js
    Formatting fixes
    Updated descriptions for "Blessed Countenance"

--- additional content/Subclass - Sorcerer (Favored Soul) [Feb2017].js
+++ additional content/Subclass - Sorcerer (Favored Soul) [Feb2017].js
@@ -31,13 +31,13 @@ ClassSubList["favoredsoul"] = {
 			name : "Divine Magic",
 			source : ["UA:S", 1],
 			minlevel : 1,
-			description : "\n   " + "When my Spellcasting feature lets me learn a sorcerer cantrip or a sorcerer spell of 1st level or higher," + "\n   " + "I can choose the new spell from the cleric spell list, in addition to the sorcerer spell list." + "\n   " + "I must otherwise obey all the restrictions for selecting the spell," + "\n   " + "and it becomes a sorcerer spell for me.",
+			description : "\n   " + "When my Spellcasting feature lets me learn a sorcerer cantrip or a sorcerer spell of 1st level or higher, I can choose the new spell from the cleric spell list, in addition to the sorcerer spell list." + "\n   " + "I must otherwise obey all the restrictions for selecting the spell, and it becomes a sorcerer spell for me.",
 		},
 		"subclassfeature1.1" : {
 			name : "Supernatural Resilience",
 			source : ["UA:S", 1],
 			minlevel : 1,
-			description : "\n   " + "My hit point maximum increases by 1," + "\n   " + "and it increases by 1 again whenever I gain a level in this class.",
+			description : "\n   " + "My hit point maximum increases by 1, and it increases by 1 again whenever I gain a level in this class.",
 			calcChanges : {
 					hp : "if (classes.known.favoredsoul) {extrahp += classes.known.favouredsoul.level; extrastring += \"\\n + \" + classes.known.favouredsoul.level + \" from Supernatural Resilience (Favored Soul)\";};"
       },
@@ -46,7 +46,7 @@ ClassSubList["favoredsoul"] = {
 			name : "Favored By the Gods",
 			source : ["UA:S", 1],
 			minlevel : 1,
-			description : "\n   " + "If I fail a saving throw or miss with an attack roll," + "\n   " + "I can roll 2d4 and add it to the total.",
+			description : "\n   " + "If I fail a saving throw or miss with an attack roll, I can roll 2d4 and add it to the total.",
       recovery : "short rest",
 		},
 		"subclassfeature6" : {
@@ -58,22 +58,22 @@ ClassSubList["favoredsoul"] = {
       "beautiful" : {
             name : "Beautiful",
             source : ["UA:S", 1],
-            description : "My appearance takes on an otherworldly quality of beauty"
+            description : "My appearance takes on an otherworldly quality of beauty" + "\n   " + "If my proficiency bonus applies to a Charisma check, I double that bonus."
       },
       "youthful" : {
             name : "Youthful",
             source : ["UA:S", 1],
-            description : "My appearance takes on an otherworldly quality of youthfulness"
+            description : "My appearance takes on an otherworldly quality of youthfulness" + "\n   " + "If my proficiency bonus applies to a Charisma check, I double that bonus."
       },
       "kind" : {
             name : "Kind",
             source : ["UA:S", 1],
-            description : "My appearance takes on an otherworldly quality of kindness"
+            description : "My appearance takes on an otherworldly quality of kindness" + "\n   " + "If my proficiency bonus applies to a Charisma check, I double that bonus."
       },
       "imposing" : {
             name : "Imposing",
             source : ["UA:S", 1],
-            description : "My appearance takes on an otherworldly quality of imposingness"
+            description : "My appearance takes on an otherworldly quality of imposingness" + "\n   " + "If my proficiency bonus applies to a Charisma check, I double that bonus."
       }, 
 		},
     "subclassfeature14" : {
@@ -87,7 +87,7 @@ ClassSubList["favoredsoul"] = {
       name: "Unearthly Recovery",
       source : ["UA:S", 1],
       minlevel : 18,
-      description : "\n   " + "I gain the ability to overcome grievous injuries." + "\n   " + "As a bonus action when I have less than half my hit points left," + "\n   " + "I can regain hit points equal to half my hit point maximum." + "\n   " + "Once I use this feature, I can't use it again until after a long rest.",
+      description : "\n   " + "I gain the ability to overcome grievous injuries." + "\n   " + "As a bonus action when I have less than half my hit points left, I can regain hit points equal to half my hit point maximum." + "\n   " + "Once I use this feature, I can't use it again until after a long rest.",
       recovery : "long rest",
       action : ["bonus action", ""]
     }    
